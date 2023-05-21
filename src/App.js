8506bb9c60b25250d2ea1a5a7531d2bba8309c26
SX <sheuxiang@gmail.com>
2023-05-21 10:06:09 +0800

    update readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useSound from "use-sound";
+import "tailwindcss/tailwind.css";
 
-function App() {
+const alphabet = [
+  "a",
+  "ă",
+  "â",
+  "b",
+  "c",
+  "d",
+  "đ",
+  "e",
+  "ê",
+  "g",
+  "h",
+  "i",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "ô",
+  "ơ",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "ư",
+  "v",
+  "x",
+  "y",
+];
+
+const LetterButton = ({ letter }) => {
+  const [play] = useSound(`sounds/${letter}.mp3`, { volume: 1 });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button
+      onClick={play}
+      className="w-16 h-16 bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl rounded-full m-3 shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
+    >
+      {`${letter.toUpperCase()} ${letter}`}
+    </button>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="flex flex-wrap justify-center items-center h-screen bg-gray-100 p-10">
+      <h1 className="w-full text-3xl text-gray-800 font-bold text-center mb-10">
+        Click to Hear Vietnamese Alphabets
+      </h1>
+      {alphabet.map((letter) => (
+        <LetterButton key={letter} letter={letter} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
